acc1b829b100d2d7f98573122b621a73923761ab
M1CH33L <mi.bandelli@duocuc.cl>
2025-06-03 19:09:19 -0400

    Commit de prueba, solo cambi un par de lineas

--- productos-service/src/main/java/com/perfulandia/productos_service/model/Producto.java
+++ productos-service/src/main/java/com/perfulandia/productos_service/model/Producto.java
@@ -14,10 +14,19 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private String marca;
+
+    @Column(nullable = false)
     private String descripcion;
+
+    @Column(nullable = false)
     private Double precio;
+
+    @Column(nullable = false)
     private Integer stock;
 }
 
