798edb51c779648fdfd6ca7b7344a8ea4d998b7d
M1CH33L <mi.bandelli@duocuc.cl>
2025-06-10 18:34:16 -0400

    productos

--- usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/model/UsuariosModel.java
+++ usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/model/UsuariosModel.java
@@ -1,4 +1,34 @@
 package com.perfulandia.usuarios_service.model;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuariosModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 10)
+    private Integer runCliente;
+
+    @Column(nullable = false, length = 1)
+    private String dvRunCLiente;
+
+    @Column(nullable = false)
+    private String nombreCompleto;
+
+    @Column(nullable = true)
+    private Integer telefono;
+
+    @Column(nullable = true)
+    private String correo;
 }
--- usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/service/UsuariosService.java
+++ usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/service/UsuariosService.java
@@ -1,4 +1,23 @@
 package com.perfulandia.usuarios_service.service;
 
+import com.perfulandia.usuarios_service.model.UsuariosModel;
+import com.perfulandia.usuarios_service.repository.UsuariosRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UsuariosService {
+
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
+    public List<usuariosModel> findAll(){
+        return usuariosRepository.findAll();
+    }
+
+
 }
