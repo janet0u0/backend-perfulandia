2f894f86d7d9f341dbe93fa0060bae4ca4fad6f1
DIEGOhidalg <die.hidalgoa@duocuc.cl>
2025-06-10 22:04:20 -0400

    Refactor de pedidos a inventario

--- productos/productos-service/src/main/java/com/perfulandia/productos_service/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.productos_service.repository;
-
-import com.perfulandia.productos_service.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-}
--- usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/controller/UsuariosController.java
+++
@@ -1,9 +0,0 @@
-package com.perfulandia.usuarios_service.controller;
-
-import com.perfulandia.usuarios_service.model.UsuariosModel;
-import com.perfulandia.usuarios_service.service.UsuariosService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-
-public class UsuariosController {
-}
