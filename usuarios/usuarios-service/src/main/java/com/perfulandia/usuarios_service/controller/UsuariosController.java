f763d5ea424a36134e4b9844156116e748904588
schwarzenbergOliva <schwarzenberg.oliva@gmail.com>
2025-06-15 22:09:55 -0400

    testeando

--- usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/controller/UsuariosController.java
+++ usuarios/usuarios-service/src/main/java/com/perfulandia/usuarios_service/controller/UsuariosController.java
@@ -6,4 +6,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class UsuariosController {
+
+    package com.perfulandia.usuarios_service.controller;
+
+import com.perfulandia.usuarios_service.model.UsuariosModel;
+import com.perfulandia.usuarios_service.service.UsuariosService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/usuarios")
+    public class UsuariosController {
+
+        @Autowired
+        private UsuariosService usuariosService;
+
+        @GetMapping
+        public List<UsuariosModel> listar() {
+            return usuariosService.listarUsuarios();
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<UsuariosModel> obtener(@PathVariable Long id) {
+            return usuariosService.obtenerUsuario(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        }
+
+        @PostMapping
+        public UsuariosModel crear(@RequestBody UsuariosModel usuario) {
+            return usuariosService.guardarUsuario(usuario);
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<UsuariosModel> actualizar(@PathVariable Long id, @RequestBody UsuariosModel usuario) {
+            UsuariosModel actualizado = usuariosService.actualizarUsuario(id, usuario);
+            if (actualizado == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(actualizado);
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+            usuariosService.eliminarUsuario(id);
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+
 }
