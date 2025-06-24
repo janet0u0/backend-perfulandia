f0d019cad75ca9be8b23d18b6aa1eff1d91274e1
schwarzenbergOliva <schwarzenberg.oliva@gmail.com>
2025-06-23 22:48:04 -0400

    .

--- productos/productos-services/src/test/java/com/perfulandia/productos_service/ProductosServiceApplicationTests.java
+++ productos/productos-services/src/test/java/com/perfulandia/productos_service/ProductosServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.perfulandia.productos_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
+
 class ProductosServiceApplicationTests {
 
 	@Test
